b96a88ea9350087f2b75f276a5a6f3c8910f866e
mike_neck <jkrt3333@gmail.com>
2018-06-02 01:16:21 +0900

    RxSwift完全にﾏｽﾀｰした

--- Hello-Rx/Sources/Hello-Rx/main.swift
+++ Hello-Rx/Sources/Hello-Rx/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import RxSwift
+import Foundation
+
+let scheduler = SerialDispatchQueueScheduler(qos: .default)
+let subscription = Observable<Int>.interval(0.3, scheduler: scheduler)
+        .subscribe( {event in
+            NSLog("event: \(event)")
+        } )
+
+Thread.sleep(forTimeInterval: 2.5)
+
+subscription.dispose()
+
+
