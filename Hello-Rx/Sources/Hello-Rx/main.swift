1210f537a16f1c2380dbc63710006935aca5a620
mike_neck <jkrt3333@gmail.com>
2018-06-02 16:16:28 +0900

    Observable 完全にﾏｽﾀｰした

--- Hello-Rx/Sources/Hello-Rx/main.swift
+++ Hello-Rx/Sources/Hello-Rx/main.swift
@@ -3,12 +3,26 @@ import Foundation
 
 let scheduler = SerialDispatchQueueScheduler(qos: .default)
 let subscription = Observable<Int>.interval(0.3, scheduler: scheduler)
-        .subscribe( {event in
+        .subscribe({ event in
             NSLog("event: \(event)")
-        } )
+        })
 
 Thread.sleep(forTimeInterval: 2.5)
 
 subscription.dispose()
 
+let semaphore = DispatchSemaphore(value: 0)
 
+let _ = Observable.of("foo")
+        .subscribe { event in NSLog("type: \(type(of: event)), event: \(String(describing: event))") }
+
+let disposeBag = DisposeBag()
+
+let subscription2 = Observable.just("suc")
+        .delay(0.01, scheduler: scheduler)
+        .subscribe(
+                onNext: { str in NSLog(str) },
+                onError: { error in NSLog("error: \(String(describing: error))") },
+                onCompleted: { () in semaphore.signal() })
+
+subscription2.disposed(by: disposeBag)
