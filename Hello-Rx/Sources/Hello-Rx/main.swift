03d0eea7e3e9cd55791c44a89305adb87b8a68b0
mike_neck <jkrt3333@gmail.com>
2018-06-02 21:57:47 +0900

    名前空間ががが

--- Hello-Rx/Sources/Hello-Rx/main.swift
+++ Hello-Rx/Sources/Hello-Rx/main.swift
@@ -2,27 +2,38 @@ import RxSwift
 import Foundation
 
 let scheduler = SerialDispatchQueueScheduler(qos: .default)
-let subscription = Observable<Int>.interval(0.3, scheduler: scheduler)
-        .subscribe({ event in
-            NSLog("event: \(event)")
-        })
 
-Thread.sleep(forTimeInterval: 2.5)
+func first() {
+    let subscription = Observable<Int>.interval(0.3, scheduler: scheduler)
+            .subscribe({ event in
+                NSLog("event: \(event)")
+            })
 
-subscription.dispose()
+    Thread.sleep(forTimeInterval: 2.5)
 
-let semaphore = DispatchSemaphore(value: 0)
+    subscription.dispose()
+}
 
-let _ = Observable.of("foo")
-        .subscribe { event in NSLog("type: \(type(of: event)), event: \(String(describing: event))") }
+func second() {
+    let _ = Observable.of("foo")
+            .subscribe { event in NSLog("type: \(type(of: event)), event: \(String(describing: event))") }
+}
 
-let disposeBag = DisposeBag()
+func third() {
+    let semaphore = DispatchSemaphore(value: 0)
 
-let subscription2 = Observable.just("suc")
-        .delay(0.01, scheduler: scheduler)
-        .subscribe(
-                onNext: { str in NSLog(str) },
-                onError: { error in NSLog("error: \(String(describing: error))") },
-                onCompleted: { () in semaphore.signal() })
+    let disposeBag = DisposeBag()
 
-subscription2.disposed(by: disposeBag)
+    let subscription2 = Observable.just("suc")
+            .delay(0.01, scheduler: scheduler)
+            .subscribe(
+                    onNext: { str in NSLog(str) },
+                    onError: { error in NSLog("error: \(String(describing: error))") },
+                    onCompleted: { () in semaphore.signal() })
+
+    subscription2.disposed(by: disposeBag)
+}
+
+first()
+second()
+third()
