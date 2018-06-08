9384126e214d5abb7c7f9fd381500de2f28f091b
mike_neck <jkrt3333@gmail.com>
2018-06-08 12:15:17 +0900

    json をちょっとためしたい

--- Hello-Rx/Package.swift
+++ Hello-Rx/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         products: [
             .executable(name: "Hello-Rx", targets: ["Hello-Rx"]),
             .library(name: "Hello-Rx-Mod", targets: ["Hello-Rx-Mod"]),
+            .executable(name: "Hello-Json", targets: ["Hello-Json"]),
         ],
         dependencies: [
             .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0"..<"5.0.0"),
@@ -16,5 +17,6 @@ let package = Package(
                     name: "Hello-Rx",
                     dependencies: ["RxSwift", "Hello-Rx-Mod"]),
             .target(name: "Hello-Rx-Mod", dependencies: ["RxSwift"]),
+            .target(name: "Hello-Json", dependencies: []),
         ]
 )
