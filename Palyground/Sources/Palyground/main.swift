48e68133412134a384a05225b651d15b14aa1752
mike_neck <jkrt3333@gmail.com>
2018-08-04 18:43:50 +0900

    DispatchSemaphore の動作確認

--- Palyground/Sources/Palyground/main.swift
+++ Palyground/Sources/Palyground/main.swift
@@ -6,32 +6,71 @@ import RxSwift
 //try! UrlRequestRunner().mainRun()
 
 
-guard let url = URL(string: "https://api.github.com/search/repositories?q=netty&sort=stars&order=desc&per_page=3") else {
-    NSLog("url error")
-    exit(1)
-}
+//guard let url = URL(string: "https://api.github.com/search/repositories?q=netty&sort=stars&order=desc&per_page=3") else {
+//    NSLog("url error")
+//    exit(1)
+//}
+//
+//let client = HttpClient(url: url)
+//
+//defer {
+//    try? client.eventLoopGroup.syncShutdownGracefully()
+//}
+//
+//let semaphore = DispatchSemaphore(value: 0)
+//
+//let future: Single<String> = client.get()
+//
+//future.subscribe { event in
+//    switch event {
+//    case .error(let e):
+//        NSLog("failed:\n\(e)")
+//        semaphore.signal()
+//    case .success(let body):
+//        NSLog("success:\n\(body)")
+//        semaphore.signal()
+//    }
+//}
+//
+//NSLog("request send.")
+//
+//semaphore.wait()
 
-let client = HttpClient(url: url)
+let semaphore = DispatchSemaphore(value: -1)
 
-defer {
-    try? client.eventLoopGroup.syncShutdownGracefully()
-}
+let single = Single<String>.create(subscribe: { single in
+    single(.success("foo"))
+    return Disposables.create()
+})
 
-let semaphore = DispatchSemaphore(value: 0)
+let bag = DisposeBag()
 
-let future: Single<String> = client.get()
+let disposable = single.map({ $0.lengthOfBytes(using: .utf8) })
+        .delay(1.0, scheduler: ConcurrentDispatchQueueScheduler(qos: .default))
+        .do(onDispose: {
+            semaphore.signal()
+            NSLog("onDispose")
+        })
+        .subscribe { event in
+            if case .success(let string) = event {
+                NSLog("success: \(string)")
+            } else if case .error(let e) = event {
+                NSLog("error: \(e)")
+            }
+            semaphore.signal()
+            NSLog("subscribe")
+        }
 
-future.subscribe { event in
-    switch event {
-    case .error(let e):
-        NSLog("failed:\n\(e)")
-        semaphore.signal()
-    case .success(let body):
-        NSLog("success:\n\(body)")
-        semaphore.signal()
-    }
-}
-
-NSLog("request send.")
+NSLog("wait 0")
 
 semaphore.wait()
+
+NSLog("wait 1")
+
+semaphore.wait()
+
+NSLog("wait 2")
+
+bag.insert(disposable)
+
+NSLog("main")
