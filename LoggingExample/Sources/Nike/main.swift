c73e8afa075b177102adb38c0817b478fbf62fc8
mike_neck <jkrt3333@gmail.com>
2019-03-29 15:30:22 +0900

    refactor Nike app

--- LoggingExample/Sources/Nike/main.swift
+++ LoggingExample/Sources/Nike/main.swift
@@ -3,47 +3,8 @@ import Foundation
 import Willow
 import Logging
 
-typealias Log = Logging.Logger
-typealias WilLogger = Willow.Logger
-
-let logger = Logger(logLevels: [LogLevel.all], writers: [ConsoleWriter(modifiers: [TimestampModifier()])])
-
-enum Msg: LogMessage {
-    case start(name: String, _ message: String)
-
-    var name: String {
-        switch self {
-        case .start(let name, _): return name
-        }
-    }
-
-    var attributes: [String: Any] {
-        switch self {
-        case .start(_, let message): return ["message": message, "phase": "start"]
-        }
-    }
-}
-
-let schemes = ["h2", "https"]
-let separators = ["\\", "://"]
-let hosts = ["localhost:2000", "example.com", "www.example.com"]
-
-for scheme in schemes {
-    logger.debug(Msg.start(name: "scheme", scheme))
-    for sep in separators {
-        logger.debug(Msg.start(name: "separator", sep))
-        for host in hosts {
-            logger.debug(Msg.start(name: "host", host))
-            guard let url = URL(string: "\(scheme)\(sep)\(host)") else {
-                logger.warnMessage("invalid url: \(scheme)\(sep)\(host)")
-                break
-            }
-            logger.infoMessage("create url: \(url)")
-        }
-    }
-}
-
-logger.infoMessage("nike-done")
+public typealias Log = Logging.Logger
+public typealias WilLogger = Willow.Logger
 
 extension Logging.Logger.Level {
 
@@ -62,7 +23,7 @@ extension Logging.Logger.Level {
         }
     }
 
-    func handle(logger: WilLogger, withMessage message: LogMessage) {
+    @inlinable func handle(logger: WilLogger, withMessage message: LogMessage) {
         switch self {
         case .debug:
             logger.debug(message)
@@ -82,6 +43,7 @@ struct WillowLogHandler: LogHandler {
 
     enum Message: LogMessage {
         case logMessage(file: String, function: String, line: UInt, metaadta: Logging.Logger.Metadata?, message: String)
+
         var name: String {
             switch self {
             case .logMessage(let file, let function, let line, _, _):
@@ -90,6 +52,7 @@ struct WillowLogHandler: LogHandler {
                        """
             }
         }
+
         var attributes: [String: Any] {
             switch self {
             case .logMessage(_, _, _, let metadata, let message):
@@ -101,17 +64,24 @@ struct WillowLogHandler: LogHandler {
         }
     }
 
-    let delegating: (Logging.Logger.Level) -> WilLogger
+    let createLogger: (Logging.Logger.Level) -> WilLogger
     var delegate: WilLogger
 
     init(delegating: @escaping (Logging.Logger.Level) -> WilLogger, logLevel: Logging.Logger.Level) {
-        self.delegating = delegating
+        self.createLogger = delegating
         self.delegate = delegating(logLevel)
         self.logLevel = logLevel
     }
 
-    func log(level: Logging.Logger.Level, message: Logging.Logger.Message, metadata: Logging.Logger.Metadata?, file: String, function: String, line: UInt) {
-        let msg = Message.logMessage(file: file, function: function, line: line, metaadta: metadata, message: String(describing: message))
+    func log(
+            level: Logging.Logger.Level,
+            message: Logging.Logger.Message,
+            metadata: Logging.Logger.Metadata?,
+            file: String,
+            function: String,
+            line: UInt) {
+        let msg = Message.logMessage(
+                file: file, function: function, line: line, metaadta: metadata, message: String(describing: message))
         level.handle(logger: delegate, withMessage: msg)
     }
 
@@ -127,53 +97,98 @@ struct WillowLogHandler: LogHandler {
     var metadata: Log.Metadata = [:]
     var logLevel: Log.Level {
         didSet {
-            self.delegate = delegating(logLevel)
+            self.delegate = createLogger(logLevel)
         }
     }
 }
 
-var apple = Log(
-        label: "com.example.App",
-        factory: { label in
-            WillowLogHandler(delegating: { level in
-                WilLogger(logLevels: level.wilowLevel(), writers: [ConsoleWriter(modifiers: [TimestampModifier()])]) }, logLevel: .info) })
 
-apple.info("start info")
+let schemes = ["h2", "https"]
+let separators = ["\\", "://"]
+let hosts = ["localhost:2000", "example.com", "www.example.com"]
 
-for scheme in schemes {
-    apple.debug("start scheme: \(scheme)")
-    for separator in separators {
-        apple.debug("start separator: \(separator)")
-        for host in hosts {
-            apple.debug("start host: \(host)")
-            let string = "\(scheme)\(separator)\(host)"
-            if let url = URL(string: string) {
-                apple.info("valid url: \(url)")
-            } else {
-                apple.error("invalid url: \(string)")
+public func runAppForWillow(use willow: WilLogger) {
+    for scheme in schemes {
+        willow.debugMessage("start scheme: \(scheme)")
+
+        for sep in separators {
+            willow.debugMessage("start separator: \(sep)")
+
+            for host in hosts {
+                willow.debugMessage("start host: \(host)")
+
+                let urlString = "\(scheme)\(sep)\(host)"
+                guard let url = URL(string: urlString) else {
+                    willow.warnMessage("invalid url: \(urlString)")
+                    break
+                }
+                willow.infoMessage("valid url: \(url)")
             }
         }
     }
 }
 
-apple.logLevel = .debug
+public func runAppForApple(use apple: Log) {
+    for scheme in schemes {
+        apple.debug("start scheme: \(scheme)")
 
-apple.info("start debug")
+        for separator in separators {
+            apple.debug("start separator: \(separator)")
 
-for scheme in schemes {
-    apple.debug("start scheme: \(scheme)")
-    for separator in separators {
-        apple.debug("start separator: \(separator)")
-        for host in hosts {
-            apple.debug("start host: \(host)")
-            let string = "\(scheme)\(separator)\(host)"
-            if let url = URL(string: string) {
-                apple.info("valid url: \(url)")
-            } else {
-                apple.error("invalid url: \(string)")
+            for host in hosts {
+                apple.debug("start host: \(host)")
+                let urlString = "\(scheme)\(separator)\(host)"
+
+                if let url = URL(string: urlString) {
+                    apple.info("valid url: \(url)")
+                } else {
+                    apple.error("invalid url: \(urlString)")
+                }
             }
         }
     }
 }
 
-apple.info("app-done")
+
+
+
+let args = CommandLine.arguments
+
+if args.count == 1 || args[1] == "willow" {
+
+    let willow = WilLogger(logLevels: LogLevel.all, writers: [ConsoleWriter(modifiers: [TimestampModifier()])])
+
+    runAppForWillow(use: willow)
+
+} else if args[1] == "info" {
+    let apple = Log(
+            label: "com.example.App",
+            factory: { label in
+                WillowLogHandler(delegating: { level in
+                    WilLogger(
+                            logLevels: level.wilowLevel(),
+                            writers: [ConsoleWriter(modifiers: [TimestampModifier()])])
+                }, logLevel: .info)
+            })
+
+    apple.info("start info")
+
+    runAppForApple(use: apple)
+
+} else if args[1] == "debug" {
+    var apple = Log(
+            label: "com.example.App",
+            factory: { label in
+                WillowLogHandler(delegating: { level in
+                    WilLogger(
+                            logLevels: level.wilowLevel(),
+                            writers: [ConsoleWriter(modifiers: [TimestampModifier()])])
+                }, logLevel: .info)
+            })
+
+    apple.info("start info")
+
+    apple.logLevel = .debug
+
+    runAppForApple(use: apple)
+}
