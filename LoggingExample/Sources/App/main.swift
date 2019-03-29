4421719975e6d4f129d3bb274a3902c0ac30f93a
mike_neck <jkrt3333@gmail.com>
2019-03-29 14:40:45 +0900

    change example code of app to be the same as others.

--- LoggingExample/Sources/App/main.swift
+++ LoggingExample/Sources/App/main.swift
@@ -1,134 +1,35 @@
 import Foundation
 import Logging
 
-struct NamedLogHandler: LogHandler {
+func runApp(use logger: Logger) {
+    let schemes = ["h2", "https"]
+    let separators = ["\\", "://"]
+    let hosts = ["localhost", "example.com"]
 
-    let label: String
-    var delegate: Logger
+    for scheme in schemes {
+        logger.debug("start scheme: \(scheme)")
 
-    init(_ label: String) {
-        self.label = label
-        self.delegate = Logger(label: label)
-    }
+        for separator in separators {
+            logger.debug("start separator: \(separator)")
 
-    func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, file: String, function: String, line: UInt) {
-        delegate.log(level: level, message, metadata: metadata, file: file, function: function, line: line)
-    }
+            for host in hosts {
+                logger.debug("start host: \(host)")
 
-    subscript(metadataKey metadataKey: String) -> Logging.Logger.Metadata.Value? {
-        get {
-            return delegate[metadataKey: metadataKey]
-        }
-        set {
-            delegate[metadataKey: metadataKey] = newValue
-        }
-    }
-    var metadata: Logger.Metadata {
-        get {
-            return delegate.metadata
-        }
-        set {
-            delegate.metadata = newValue
-        }
-    }
-    var logLevel: Logger.Level {
-        get {
-            return delegate.logLevel
-        }
-        set {
-            delegate.logLevel = newValue
+                let urlString = "\(scheme)\(separator)\(host)"
+                if let url = URL(string: urlString) {
+                    logger.info("valid url: \(url)")
+                } else {
+                    logger.error("invalid url: \(urlString)")
+                }
+            }
         }
     }
 }
 
-struct LoggerFactory {
-    static func getLogger(_ label: String) -> Logger {
-        let lgr = Logger(label: label, factory:{ str in NamedLogHandler(label) })
-        return lgr
-    }
-}
+var logger = Logger(label: "com.example.App")
 
+runApp(use: logger)
 
-typealias UserId = Int
+logger.logLevel = .info
 
-struct User: CustomStringConvertible, Decodable, Encodable {
-
-    private static var logger = Logger(label: "com.example.User")
-
-    let id: UserId
-    let name: String
-    let address: String
-
-    init(_ id: UserId, name: String, address: String) {
-        self.id = id
-        self.name = name
-        self.address = address
-    }
-
-    var description: String {
-        return """
-               User[name: "\(name)", address: "\(address)"]
-               """
-    }
-
-    func greeting(to user: User) {
-        User.logger.info(
-                """
-                Hello from \(self) to \(user)
-                """)
-    }
-
-    func receive(data : Data) {
-        let json = String(data: data, encoding: .utf8)
-
-        let decoder = JSONDecoder()
-        do {
-            let user = try decoder.decode(User.self, from: data)
-            User.logger.info("success: receive message[\(json)] from \(user)")
-        } catch {
-            User.logger.warning("failure: receive unknown message[\(json)], \(error)")
-        }
-    }
-
-    func sendMessage(to user: User) {
-        let encoder = JSONEncoder()
-        do {
-            let json = try encoder.encode(self)
-            user.receive(data: json)
-            let jsonString = String(data: json, encoding: .utf8)
-            User.logger.info("success: sent message[\(jsonString)] to \(user)")
-        } catch {
-            User.logger.error("failure: sent message to \(user), \(error)")
-        }
-    }
-}
-
-var logger = LoggerFactory.getLogger("com.example.Main")
-
-logger.logLevel = .debug
-logger[metadataKey: "operator"] = "master-user"
-
-logger.info("start application")
-
-logger.debug("ユーザーを作る")
-let 太郎 = User(100, name: "太郎", address: "東京")
-let 花子 = User(300, name: "花子", address: "大阪")
-
-logger.info("greeting from 花子 to 太郎")
-
-花子.greeting(to: 太郎)
-
-logger.info("message from 太郎 to 花子")
-
-太郎.sendMessage(to: 花子)
-
-logger.info("花子 receive unknown message from stranger")
-
-logger.debug("不審なデータ送る")
-let unknownMessage = "[こここふふふ]"
-guard let data = unknownMessage.data(using: .utf8) else {
-    logger.error("error to convert data: \(unknownMessage)")
-    exit(1)
-}
-
-花子.receive(data: data)
+runApp(use: logger)
